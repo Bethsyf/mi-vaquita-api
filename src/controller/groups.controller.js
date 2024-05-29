a2afdf6f5563c1653436e62bb87cfa5eb6ff2896
bethsyf <bethsyfalcon@gmail.com>
2024-05-28 19:10:52 -0500

    implementation joi in groups

--- src/controller/groups.controller.js
+++ src/controller/groups.controller.js
@@ -1,4 +1,5 @@
 import { GroupsServices } from '../services/groups.service.js';
+import Joi from 'joi';
 
 const GroupController = () => {
   const groupsService = GroupsServices();
@@ -7,74 +8,81 @@ const GroupController = () => {
     try {
       const groups = await groupsService.getAll();
       return res.status(200).json({
-        message: 'groups retrieved successfully',
+        message: 'Groups retrieved successfully',
         groups,
       });
     } catch (error) {
-      res
-        .status(500)
-        .json({ error: 'Error fetching groups: ' + error.message });
+      return handleError(res, error, 'Error fetching groups');
     }
   };
 
   const getById = async (req, res) => {
     try {
-      const id = req.params.id;
-      const group = await groupsService.getById(id);
-      if (!group) return res.status(404).send('Group not found');
+      const group = await groupsService.getById(req.params.id);
+      if (!group) {
+        return res.status(404).send('Group not found');
+      }
       return res.status(200).json(group);
     } catch (error) {
-      res
-        .status(500)
-        .json({ error: 'Error fetching groups: ' + error.message });
+      return handleError(res, error, 'Error fetching group');
     }
   };
 
   const create = async (req, res) => {
-    if (!req.body.name || req.body.name.length > 30) {
-      return res.status(400).json({
-        error: 'Name is required and must be less than 30 characters',
-      });
-    }
-
     try {
+      await validateGroup(req.body);
       const newGroup = await groupsService.create(
         req.body.name,
         req.body.color
       );
-      if (!newGroup) {
-        return res.status(400).json({ error: 'Group already exists' });
-      }
-      res.status(201).json(newGroup);
+      return res.status(201).json(newGroup);
     } catch (error) {
-      res.status(500).json({ error: 'Error creating group: ' + error.message });
+      return handleError(res, error, 'Error creating group');
     }
   };
 
   const update = async (req, res) => {
     try {
-      const id = req.params.id;
-      const updatedFields = req.body;
-      const updatedGroup = await groupsService.update(id, updatedFields);
-      if (!updatedGroup)
+      await validateGroup(req.body);
+      const updatedGroup = await groupsService.update(req.params.id, req.body);
+      if (!updatedGroup) {
         return res.status(404).json({ error: 'Group not found' });
-      res.status(200).json(updatedGroup);
+      }
+      return res.status(200).json(updatedGroup);
     } catch (error) {
-      res.status(500).json({ error: 'Error updating group: ' + error.message });
+      return handleError(res, error, 'Error updating group');
     }
   };
 
   const removeById = async (req, res) => {
     try {
-      const id = req.params.id;
-      const deleted = await groupsService.removeById(id);
+      const deleted = await groupsService.removeById(req.params.id);
       if (!deleted) {
         return res.status(404).json({ error: 'Group not found' });
       }
-      res.status(200).json({ message: 'Group deleted successfully' });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
+      return res.status(200).json({ message: 'Group deleted successfully' });
+    } catch (error) {
+      return handleError(res, error, 'Error deleting group');
     }
+  };
+
+  const validateGroup = async (group) => {
+    const schema = Joi.object({
+      name: Joi.string().min(3).max(20).required(),
+      color: Joi.string().required(),
+    });
+
+    const existingGroup = await groupsService.getByName(group.name);
+    if (existingGroup) {
+      throw new Error('Group with the same name already exists');
+    }
+
+    await schema.validateAsync(group);
+  };
+
+  const handleError = (res, error, message) => {
+    console.error(message + ':', error.message);
+    return res.status(500).json({ error: message + ': ' + error.message });
   };
 
   return {
